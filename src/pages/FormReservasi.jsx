45c7351daf5d7167b2c399c00d81f364a466dd9a
Veri Ramadhan <93845135+razoraz@users.noreply.github.com>
2026-01-04 07:14:23 +0700

    Update FormReservasi.jsx

--- src/pages/FormReservasi.jsx
+++ src/pages/FormReservasi.jsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import HeaderPagePelanggan from '../components/component-html/HeaderPagePelanggan';
-import FooterPage from '../components/component-html/FooterPage';
+import FooterPage from '../components/component-html/FooterPagePelanggan';
 import Modal from '../components/modal-notifikasi/ModalNotifikasi';
 
 import styles from '../styles/form_reservasi.module.css';
